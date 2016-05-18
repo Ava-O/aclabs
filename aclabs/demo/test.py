28577912b404924825ef4f4a08ded56b62ea9b04
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2016-05-18 18:17:37 +0300

    My first commit
    Just basic stuff

---
+++ aclabs/demo/test.py
@@ -0,0 +1,31 @@
+# Some comment
+# My first python app
+'''
+Setting up Python path on Windows:
+$pythonPath = 'C:\Python27'
+$env:Path += ";$pythonPath"
+setx PATH $env:Path /M
+'''
+
+a = 10
+my_string = 'test'
+my_string = "test2"
+
+# # import six.moves
+# c = int(raw_input('Number: '))
+# print('Your number %s' % c)
+# print(c + a)
+
+# my_dict = {'test': 10,
+#            'abc': [1, 2, 'test', 'test3'],
+#            'c': dict(a=10),
+#            'cd': {'a': 10}}
+# print my_dict
+# print my_dict['abc']
+# my_dict['c'] = 'a'
+
+# print dir(my_dict)
+# a = range(10)
+# print a[2:-1:2]
+# print a[0]
+# my_string[1] = 't'
